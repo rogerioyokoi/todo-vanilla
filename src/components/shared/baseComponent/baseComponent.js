b40ff7f69ac533e33f377f2f787d84d3018d2424
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-06-04 17:03:44 -0300

    feat: update base component and test file

--- src/components/shared/baseComponent/baseComponent.js
+++ src/components/shared/baseComponent/baseComponent.js
@@ -1,6 +1,10 @@
 /**
  * Base class for creating custom HTML elements with common utility methods.
  *
+ * @autor Rogério O. Yokoi
+ * @website https://github.com/rogerioyokoi
+ * @repository https://github.com/rogerioyokoi/todo-vanilla
+ *
  * @example
  * Example of usage:
  *
@@ -24,25 +28,101 @@ export class BaseComponent extends HTMLElement {
   /**
    * Creates an instance of BaseComponent.
    * @param {string} element - The HTML element to create.
+   * @param {boolean} [useShadowDom=false] - Flag indicating whether to use Shadow DOM.
    * @throws Will throw an error if the element is not defined.
    * @example
-   * const myComponent = new MyComponent();
+   * // Creating a component without Shadow DOM
+   * const myComponentWithoutShadowDom = new MyComponent('div');
+   *
+   * // Creating a component with Shadow DOM
+   * const myComponentWithShadowDom = new MyComponent('div', true);
    */
-  constructor(element) {
+  constructor(element, useShadowDom = false) {
     super();
     if (!element) {
       throw new Error('Define a valid HTML element to create');
     }
+
     this._element = document.createElement(element);
+
+    if (useShadowDom) {
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.appendChild(this._element);
+    } else {
+      this.appendChild(this._element);
+    }
   }
 
   /**
-   * Returns the created element.
+   * Adds custom styles to the component.
+   * @param {string} styles - The CSS styles to apply to the component.
+   * @example
+   * myComponent.addStyles('color: blue; font-size: 16px;');
+   */
+  addStyles(styles) {
+    if (this.shadowRoot) {
+      const styleElement = document.createElement('style');
+      styleElement.textContent = styles;
+      this.shadowRoot.appendChild(styleElement);
+    } else {
+      console.warn('Shadow DOM is not enabled. Styles cannot be added.');
+    }
+  }
+
+  /**
+   * Sets the inner HTML content of the main element.
+   * @param {string} innerHTML - The HTML content to insert into the main element.
+   * @example
+   * // Assuming this._element is a <div>
+   * myComponent.setInnerHTML('<p>Hello, world!</p>');
+   * // The content of this._element will be: <div><p>Hello, world!</p></div>
+   */
+  setInnerHTML(innerHTML) {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = innerHTML;
+    } else {
+      this._element.innerHTML = innerHTML;
+    }
+  }
+
+  /**
+   * Appends a new child node to the main element.
+   * @param {Node} nodeElement - The node to append as a child to the main element.
+   * @example
+   * // Assuming this._element is a <div>
+   * const newElement = document.createElement('span');
+   * newElement.textContent = 'New content';
+   * myComponent.setAppendChild(newElement);
+   * // The content of this._element will be: <div><span>New content</span></div>
+   */
+  setAppendChild(nodeElement) {
+    this._element.appendChild(nodeElement);
+  }
+
+  /**
+   * Adds a link to an external CSS file to the component.
+   * @param {string} href - The URL of the external CSS file.
+   * @example
+   * myComponent.addStyleSheet('path/to/styles.css');
+   */
+  addStyleSheet(href) {
+    if (this.shadowRoot) {
+      const linkElement = document.createElement('link');
+      linkElement.setAttribute('rel', 'stylesheet');
+      linkElement.setAttribute('href', href);
+      this.shadowRoot.appendChild(linkElement);
+    } else {
+      console.warn('Shadow DOM is not enabled. Stylesheets cannot be added.');
+    }
+  }
+
+  /**
+   * Renders the component and returns the created element.
    * @returns {HTMLElement} The created HTML element.
    * @example
-   * const element = myComponent.get();
+   * const element = myComponent.render();
    */
-  get() {
+  render() {
     return this._element;
   }
 
--- src/components/shared/baseComponent/baseComponent.test.js
+++ src/components/shared/baseComponent/baseComponent.test.js
@@ -1,115 +1,325 @@
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { axe } from 'vitest-axe';
-import { BaseComponent } from "./baseComponent";
+import { BaseComponent } from './baseComponent';
 
-describe('baseComponent', () => {
-
+describe('Test suite for the BaseComponent class', () => {
   let baseComponent;
+  let baseComponentWithShadow;
+
+  const getComponent = (withShadowDom = false) => (withShadowDom ? baseComponentWithShadow : baseComponent);
+
+  const consoleMock = vi.spyOn(console, 'warn');
 
   beforeAll(() => {
     window.customElements.define('base-component', BaseComponent);
     document.body.innerHTML = '';
-  })
+  });
 
   beforeEach(() => {
     baseComponent = new BaseComponent('div');
     document.body.appendChild(baseComponent);
+
+    baseComponentWithShadow = new BaseComponent('div', true);
+    document.body.appendChild(baseComponentWithShadow);
   });
 
   afterEach(() => {
     document.body.removeChild(baseComponent);
+    document.body.removeChild(baseComponentWithShadow);
+    consoleMock.mockReset();
   });
 
-  it('Should create a valid HTML element', () => {
-    expect(baseComponent.get()).toBeInstanceOf(HTMLDivElement);
-  });
-  // @todo
-  // Fix: test to catch a throw error.
-  it.skip('Should throw error if invalid HTML element is provided', () => {
-    expect(new BaseComponent()).toThrow('Define a valid HTML element to create');
+  it('Should throw error if invalid HTML element is provided', () => {
+    expect(() => new BaseComponent()).toThrow('Define a valid HTML element to create');
   });
 
-  it('Should set ID of the element', () => {
-    baseComponent.setId('test-id');
-    expect(baseComponent.get().id).toBe('test-id');
+  describe('Should validate a shadowDOM instance', () => {
+    it('Without shadow DOM', () => {
+      expect(getComponent().shadowRoot).toBeNull();
+    });
+
+    it('With shadow DOM', () => {
+      expect(getComponent(true).shadowRoot).not.toBeNull();
+    });
   });
 
-  it('Should remove ID from the element', () => {
-    baseComponent.setId('test-id');
-    baseComponent.removeId();
-    expect(baseComponent.get().id).toBe('');
+  describe('Should create a valid HTML element', () => {
+    it('Without shadow DOM', () => {
+      expect(getComponent().render()).toBeInstanceOf(HTMLDivElement);
+    });
+
+    it('With shadow DOM', () => {
+      expect(getComponent(true).render()).toBeInstanceOf(HTMLDivElement);
+    });
   });
 
-  it('Should pass axe accessibility tests', async () => {
-    const results = await axe(baseComponent);
-    expect(results).toHaveNoViolations();
+  describe('Should set ID of the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().setId('test-id');
+      expect(getComponent().render().id).toBe('test-id');
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).setId('test-id');
+      expect(getComponent(true).render().id).toBe('test-id');
+    });
   });
 
-  it('Should add class to the element', () => {
-    baseComponent.addClass('test-class');
-    expect(baseComponent.hasClass('test-class')).toBe(true);
+  describe('Should remove ID from the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().setId('test-id');
+      getComponent().removeId();
+      expect(getComponent().render().id).toBe('');
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).setId('test-id');
+      getComponent(true).removeId();
+      expect(getComponent(true).render().id).toBe('');
+    });
   });
 
-  it('Should add multiple classes to the element', () => {
-    baseComponent.addClass(['test-class-1', 'test-class-2']);
-    expect(baseComponent.hasClass('test-class-1')).toBe(true);
-    expect(baseComponent.hasClass('test-class-2')).toBe(true);
+  describe('Should pass axe accessibility tests', () => {
+    it('Without shadow DOM', async () => {
+      const results = await axe(baseComponent);
+      expect(results).toHaveNoViolations();
+    });
+
+    it('With shadow DOM', async () => {
+      const results = await axe(getComponent(true));
+      expect(results).toHaveNoViolations();
+    });
   });
 
-  it('Should clear all classes from the element', () => {
-    baseComponent.addClass('test-class');
-    baseComponent.clearClasses();
-    expect(baseComponent.hasClass('test-class')).toBe(false);
+  describe('Should add class to the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass('test-class');
+      expect(getComponent().hasClass('test-class')).toBe(true);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass('test-class');
+      expect(getComponent(true).hasClass('test-class')).toBe(true);
+    });
   });
 
-  it('Should check if the element has a class', () => {
-    baseComponent.addClass('test-class');
-    expect(baseComponent.hasClass('test-class')).toBe(true);
+  describe('Should add multiple classes to the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass(['test-class-1', 'test-class-2']);
+      expect(getComponent().hasClass('test-class-1')).toBe(true);
+      expect(getComponent().hasClass('test-class-2')).toBe(true);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass(['test-class-1', 'test-class-2']);
+      expect(getComponent(true).hasClass('test-class-1')).toBe(true);
+      expect(getComponent(true).hasClass('test-class-2')).toBe(true);
+    });
   });
 
-  it('Should get all classes of the element', () => {
-    baseComponent.addClass(['class-1', 'class-2']);
-    expect(baseComponent.getClasses().length).toBe(2);
+  describe('Should clear all classes from the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass('test-class');
+      getComponent().clearClasses();
+      expect(getComponent().hasClass('test-class')).toBe(false);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass('test-class');
+      getComponent(true).clearClasses();
+      expect(getComponent(true).hasClass('test-class')).toBe(false);
+    });
   });
 
-  it('Should remove a class from the element', () => {
-    baseComponent.addClass('test-class');
-    baseComponent.removeClass('test-class');
-    expect(baseComponent.hasClass('test-class')).toBe(false);
+  describe('Should check if the element has a class', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass('test-class');
+      expect(getComponent().hasClass('test-class')).toBe(true);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass('test-class');
+      expect(getComponent(true).hasClass('test-class')).toBe(true);
+    });
   });
 
-  it('Should replace a class on the element', () => {
-    baseComponent.addClass('old-class');
-    baseComponent.replaceClass('old-class', 'new-class');
-    expect(baseComponent.hasClass('old-class')).toBe(false);
-    expect(baseComponent.hasClass('new-class')).toBe(true);
+  describe('Should get all classes of the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass(['class-1', 'class-2']);
+      expect(getComponent().getClasses().length).toBe(2);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass(['class-1', 'class-2']);
+      expect(getComponent(true).getClasses().length).toBe(2);
+    });
   });
 
-  it('Should toggle a class on the element', () => {
-    baseComponent.toggleClass('toggle-class');
-    expect(baseComponent.hasClass('toggle-class')).toBe(true);
-    baseComponent.toggleClass('toggle-class');
-    expect(baseComponent.hasClass('toggle-class')).toBe(false);
+  describe('Should remove a class from the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass('test-class');
+      getComponent().removeClass('test-class');
+      expect(getComponent().hasClass('test-class')).toBe(false);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass('test-class');
+      getComponent(true).removeClass('test-class');
+      expect(getComponent(true).hasClass('test-class')).toBe(false);
+    });
   });
 
-  it('should check if the element has an attribute', () => {
-    baseComponent.setAttribute('data-attr', 'value');
-    expect(baseComponent.hasAttribute('data-attr')).toBe(true);
+  describe('Should replace a class on the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().addClass('old-class');
+      getComponent().replaceClass('old-class', 'new-class');
+      expect(getComponent().hasClass('old-class')).toBe(false);
+      expect(getComponent().hasClass('new-class')).toBe(true);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).addClass('old-class');
+      getComponent(true).replaceClass('old-class', 'new-class');
+      expect(getComponent(true).hasClass('old-class')).toBe(false);
+      expect(getComponent(true).hasClass('new-class')).toBe(true);
+    });
   });
 
-  it('should get the value of an attribute', () => {
-    baseComponent.setAttribute('data-attr', 'value');
-    expect(baseComponent.getAttributeValue('data-attr')).toBe('value');
+  describe('Should toggle a class on the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().toggleClass('toggle-class');
+      expect(getComponent().hasClass('toggle-class')).toBe(true);
+      getComponent().toggleClass('toggle-class');
+      expect(getComponent().hasClass('toggle-class')).toBe(false);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).toggleClass('toggle-class');
+      expect(getComponent(true).hasClass('toggle-class')).toBe(true);
+      getComponent(true).toggleClass('toggle-class');
+      expect(getComponent(true).hasClass('toggle-class')).toBe(false);
+    });
   });
 
-  it('should set an attribute on the element', () => {
-    baseComponent.setAttribute('data-attr', 'value');
-    expect(baseComponent.getAttributeValue('data-attr')).toBe('value');
+  describe('Should check if the element has an attribute', () => {
+    it('Without shadow DOM', () => {
+      getComponent().setAttribute('data-attr', 'value');
+      expect(getComponent().hasAttribute('data-attr')).toBe(true);
+    });
+    it('With shadow DOM', () => {
+      getComponent(true).setAttribute('data-attr', 'value');
+      expect(getComponent(true).hasAttribute('data-attr')).toBe(true);
+    });
   });
 
-  it('should remove an attribute from the element', () => {
-    baseComponent.setAttribute('data-attr', 'value');
-    baseComponent.removeAttribute('data-attr');
-    expect(baseComponent.hasAttribute('data-attr')).toBe(false);
+  describe('Should get the value of an attribute', () => {
+    it('Without shadow DOM', () => {
+      getComponent().setAttribute('data-attr', 'value');
+      expect(getComponent().getAttributeValue('data-attr')).toBe('value');
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).setAttribute('data-attr', 'value');
+      expect(getComponent(true).getAttributeValue('data-attr')).toBe('value');
+    });
+  });
+
+  describe('Should set an attribute on the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().setAttribute('data-attr', 'value');
+      expect(getComponent().getAttributeValue('data-attr')).toBe('value');
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).setAttribute('data-attr', 'value');
+      expect(getComponent(true).getAttributeValue('data-attr')).toBe('value');
+    });
+  });
+
+  describe('Should remove an attribute from the element', () => {
+    it('Without shadow DOM', () => {
+      getComponent().setAttribute('data-attr', 'value');
+      getComponent().removeAttribute('data-attr');
+      expect(getComponent().hasAttribute('data-attr')).toBe(false);
+    });
+
+    it('With shadow DOM', () => {
+      getComponent(true).setAttribute('data-attr', 'value');
+      getComponent(true).removeAttribute('data-attr');
+      expect(getComponent(true).hasAttribute('data-attr')).toBe(false);
+    });
+  });
+
+  describe('Should add custom styles to the element', () => {
+    it('Without shadow DOM', () => {
+      const styles = 'color: blue; font-size: 16px;';
+      getComponent().addStyles(styles);
+
+      expect(getComponent().shadowRoot).toBeNull();
+      expect(consoleMock).toBeCalledWith('Shadow DOM is not enabled. Styles cannot be added.');
+    });
+
+    it('With shadow DOM', () => {
+      const styles = 'color: blue; font-size: 16px;';
+      getComponent(true).addStyles(styles);
+      const styleElement = getComponent(true).shadowRoot.querySelector('style');
+      expect(styleElement.textContent).toBe(styles);
+    });
+  });
+
+  describe('Should add a link to an external stylesheet to the element', () => {
+    it('Without shadow DOM', () => {
+      const href = 'path/to/styles.css';
+      getComponent().addStyleSheet(href);
+
+      expect(getComponent().shadowRoot).toBeNull();
+      expect(consoleMock).toBeCalledWith('Shadow DOM is not enabled. Stylesheets cannot be added.');
+    });
+
+    it('With shadow DOM', () => {
+      const href = 'path/to/styles.css';
+      getComponent(true).addStyleSheet(href);
+      const linkElement = getComponent(true).shadowRoot.querySelector('link');
+      expect(linkElement.getAttribute('rel')).toBe('stylesheet');
+      expect(linkElement.getAttribute('href')).toBe(href);
+    });
+  });
+
+  describe('Should insert innerHTML on component', () => {
+    it('Without shadow DOM', () => {
+      const innerHTML = '<p>Hello, world!</p>';
+      getComponent().setInnerHTML(innerHTML);
+
+      expect(getComponent().render().innerHTML).toBe(innerHTML);
+    });
+
+    it('With shadow DOM', () => {
+      const innerHTML = '<p>Hello, world!</p>';
+      getComponent(true).setInnerHTML(innerHTML);
+
+      expect(getComponent(true).shadowRoot.innerHTML).toBe(innerHTML);
+    });
+  });
+
+  describe('Should append a new child node to the main element', () => {
+    it('Without shadow DOM', () => {
+      const component = getComponent();
+      const newElement = document.createElement('span');
+      newElement.textContent = 'New content';
+      component.setAppendChild(newElement);
+
+      expect(component._element.contains(newElement)).toBe(true);
+      expect(component._element.querySelector('span').textContent).toBe('New content');
+    });
+
+    it('With shadow DOM', () => {
+      const component = getComponent(true);
+      const newElement = document.createElement('span');
+      newElement.textContent = 'New content';
+      component.setAppendChild(newElement);
+
+      expect(component._element.contains(newElement)).toBe(true);
+      expect(component._element.querySelector('span').textContent).toBe('New content');
+    });
   });
 });
