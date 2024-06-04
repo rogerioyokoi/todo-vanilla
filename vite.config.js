97052318e3f1a8a0dd42bd0c629ae8a8eba78d92
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-06-04 17:02:56 -0300

    chore: add vite path config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 /** @type {import('vite').UserConfig} */
-import dns from 'node:dns'
-import { defineConfig } from 'vite'
+import dns from 'node:dns';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   server: {
-    port: 8080
-  }
-})
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@web-component': path.resolve(__dirname, './src/components'),
+    },
+  },
+});
